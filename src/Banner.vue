0945e5262e5aa7ed631662131531e7987e3824e3
Josiah Donor <jdonor@gmail.com>
2018-07-10 20:33:47 -0700

    Shorten banner text

--- src/Banner.vue
+++ src/Banner.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="banner" class="p-4 bg-green-dark text-grey-lightest">
-    <h2>Hi there! Feel free to poke around the site and get to know me</h2>
+    <h2>Hi there!</h2>
     <p class="mt-2">Shoot me a note: <a href="mailto:josiahdonor@gmail.com" class="text-grey-lightest">josiahdonor@gmail.com</a></p>
   </div>
 </template>
