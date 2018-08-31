5de88d5e6ee87bfa4a2acb8f7ee2aa0672c536d2
Josiah Donor <jdonor@gmail.com>
2018-08-31 13:59:49 -0700

    Add navigation bar
    Links to pages, so far there are shells for the Work History and
    Projects pages.

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -2,25 +2,27 @@ import React from 'react';
 import Link from 'gatsby-link';
 
 const Navigation = () => (
-  <ul>
-    <li>
-      <Link
-        to="/work-history"
-        style={{
-          color: 'white',
-          textDecoration: 'none',
-        }}
-      >
-        Work history
-      </Link>
-    </li>
+  <div id="nav" style={{ backgroundColor: 'rebeccapurple' }}>
+    <ul>
+      <li>
+        <Link
+          to="/work-history"
+          style={{
+            color: 'white',
+            textDecoration: 'none',
+          }}
+        >
+          Work History
+        </Link>
+      </li>
 
-    <li>
-      <Link to="/projects" style={{ color: 'white', textDecoration: 'none' }}>
-        Projects
-      </Link>
-    </li>
-  </ul>
+      <li>
+        <Link to="/projects" style={{ color: 'white', textDecoration: 'none' }}>
+          Projects
+        </Link>
+      </li>
+    </ul>
+  </div>
 );
 
 export default Navigation;
