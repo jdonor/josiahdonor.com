12d96b75f9868337e47106c01215fd8d964f087b
Josiah Donor <jdonor@gmail.com>
2018-11-19 13:20:23 -0800

    Add headshot to home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import headshot from '../img/headshot.jpg';
 
 const IndexPage = () => (
   <div>
-    <h3>Husband - Dad - Software Engineer - Musician - Gamer</h3>
-    <p>
-      Hey there! Get in touch with me here:&nbsp;
-      <a href="mailto:josiahdonor@gmail.com">josiahdonor@gmail.com</a>
-    </p>
+    <div style={{ float: 'left' }}>
+      <img src={headshot} width="200px" alt="Man's face" />
+    </div>
+    <div style={{ padding: '8px', marginLeft: '200px' }}>
+      <h3>Husband - Dad - Software Engineer - Musician - Gamer</h3>
+      <p>
+        Hey there! Get in touch with me here:&nbsp;
+        <a href="mailto:josiahdonor@gmail.com">josiahdonor@gmail.com</a>
+      </p>
+    </div>
   </div>
 );
 
